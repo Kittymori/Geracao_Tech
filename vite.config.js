e05dd3fd5addb06b705cddf4979cf62d3b770942
Kittymori <catarinaelissa@gmail.com>
2025-07-12 01:41:27 +0000

    fix: Corrigir todos os caminhos de importacao e alias Vite com base na estrutura de repositorio raiz e Root Dir de Render '.' e ortografia exata

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { ifError } from 'assert';
 
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      'src': path.resolve(__dirname, './src'),
+    },
+  },
+});
